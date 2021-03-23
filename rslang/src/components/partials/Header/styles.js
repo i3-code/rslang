eebf36c29c8c42e872908f1bafc92c3599f26f2b
volha010892 <66121147+volha010892@users.noreply.github.com>
2021-03-23 22:18:32 +0300

    refactor: change header

--- rslang/src/components/partials/Header/styles.js
+++ rslang/src/components/partials/Header/styles.js
@@ -1,56 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  appbar:{
-    background: 'white',
-    position: 'static',
-  },
-  svgicon:{
-    [theme.breakpoints.up('lg')]: {
-    fontSize: 60
-    },
-    [theme.breakpoints.between('md', 'lg')]: {
-      fontSize: 60
-    },
-    [theme.breakpoints.between('sm', 'md')]: {
-      fontSize: 50
-      },
-      [theme.breakpoints.down('sm')]: {
-        fontSize: 30
-      },
-  },
-  title: {
-    flexGrow: 1,
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  signupLink: {
-    color: 'white',
-    textDecoration: 'none',
-    marginLeft: 20,
-  },
   collapse: {
     display: 'flex',
     [theme.breakpoints.down("sm")]: {
       display: "none"
     },
   },
-  smallScreen: {
-    [theme.breakpoints.up("md")]: {
-      display: "none"
-    },
-  },
-  iconButton: {
-    alignSelf: 'center'
-  },
-  drawer: {
-    minWidth: 250,
-    maxWidth: 300,
-    overflow: 'hidden',
-    display: 'flex',
-    flexDirection: 'column'
-  }
 }));
 
 export default useStyles;
