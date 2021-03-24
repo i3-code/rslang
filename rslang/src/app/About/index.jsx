8a71639664bcd7b065464057f9ad6d87cba68214
volha010892 <66121147+volha010892@users.noreply.github.com>
2021-03-24 22:12:19 +0300

    fead: add about team

--- rslang/src/app/About/index.jsx
+++ rslang/src/app/About/index.jsx
@@ -1,33 +1,66 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+
+import CardTeam from '../../components/partials/CardTeam';
+
+import { Grid } from '@material-ui/core';
 
 export default function About() {
+  const teamArray = [
+    {
+      name: 'Игорь Захаров',
+      img: './images/team/ig.jpg',
+      position: 'Team Lead',
+      description: 'kjnjklnjlkjnlk',
+      git: 'i3-code',
+    },
+    {
+      name: 'Евгений Колоницкий',
+      img: './images/team/zen.jpg',
+      position: 'Front-end developer',
+      description: 'kjnjklnjlkjnlk',
+      git: 'drd1esel',
+    },
+    {
+      name: 'Кристина Кашевар',
+      img: './images/team/kris.jpg',
+      position: 'Front-end developer',
+      description: 'kjnjklnjlkjnlk',
+      git: 'christina-kashevar',
+    },
+    {
+      name: 'Ольга Хмыль',
+      img: './images/team/ol.jpeg',
+      position: 'Front-end developer',
+      description: 'kjnjklnjlkjnlk',
+      git: 'volha010892',
+    },
+    {
+      name: 'Денис Корзун',
+      img: './images/team/den.jpeg',
+      position: 'Front-end developer',
+      description: 'kjnjklnjlkjnlk',
+      git: 'dzianiskor',
+    },
+    {
+      name: 'Филипп Бурш',
+      img: './images/team/fil.jpg',
+      position: 'Front-end developer',
+      description: 'kjnjklnjlkjnlk',
+      git: 'heliken',
+    },
+  ];
   return (
-    <div>
-      <Typography variant="h4">About</Typography>
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-        facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-        gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-        donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-        Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-        imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-        arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-        donec massa sapien faucibus et molestie ac.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-        facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-        tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-        consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-        hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-        tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-        nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-        accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-      </Typography>
-    </div>
+    <Grid container direction="row" justify="space-evenly" alignItems="center">
+      {teamArray.map((el, i) => (
+        <CardTeam
+          key={i}
+          name={el.name}
+          img={el.img}
+          position={el.position}
+          description={el.description}
+          git={el.git}
+        />
+      ))}
+    </Grid>
   );
 }
