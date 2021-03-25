b80cc56bc2710d25d95193d6fa45bdc2bbbfd866
Heliken <burshf@gmail.com>
2021-03-25 12:03:46 +0300

    feat: add basic game logic

--- rslang/src/components/SprintGame/index.jsx
+++ rslang/src/components/SprintGame/index.jsx
@@ -1,7 +1,7 @@
-import {useState, useEffect} from 'react';
-import { render } from "@testing-library/react";
-import {Box, Typography, Grid, Button} from '@material-ui/core';
-
+import { useState, useEffect } from 'react';
+import { render } from '@testing-library/react';
+import { Box, Typography, Grid, Button } from '@material-ui/core';
+import data from './data-example';
 // source: https://stackoverflow.com/questions/57137094/implementing-a-countdown-timer-in-react-with-hooks
 const Timer = ({ seconds }) => {
   const [timeLeft, setTimeLeft] = useState(seconds);
@@ -14,33 +14,69 @@ const Timer = ({ seconds }) => {
     }, 1000);
 
     return () => clearInterval(intervalId);
-
   }, [timeLeft]);
 
-  return (
-    <div>{timeLeft}</div>
-  );
+  return <div>{timeLeft}</div>;
 };
-const Streak = () => {
-  return(
-    <div>streak indicator block</div>
-  )
-}
-const SprintGame = ( ) => {
-  const [word, setWord] = useState(null);
-  const [translation, setTranslation] = useState(null);
-  return(
+
+const Streak = ({ streak }) => {
+  return <div>{streak}</div>;
+};
+
+const SprintGame = () => {
+  const generateNewWord = (index) => {
+    const typing = data[index].word;
+    const randomTranslate = data[Math.floor(Math.random() * data.length)].wordTranslate;
+    const correctTranslate = data[index].wordTranslate;
+    const shownTranslate = Math.random() < 0.5 ? data[index].wordTranslate : randomTranslate;
+    return { typing, correctTranslate, shownTranslate };
+  };
+
+  const [counter, setCounter] = useState(0);
+  const [streak, setStreak] = useState(0);
+  const [word, setWord] = useState(generateNewWord(0));
+
+  const pickNextWord = () => {
+    setCounter(counter + 1);
+  };
+
+  const checkAnswer = (suggestedAsCorrect) => {
+    const shownTranslationIsCorrect = word.shownTranslate === word.correctTranslate;
+    const userWasCorrect =
+      (shownTranslationIsCorrect && suggestedAsCorrect) || (!shownTranslationIsCorrect && !suggestedAsCorrect);
+    userWasCorrect ? setStreak(streak + 1) : setStreak(0);
+    pickNextWord();
+  };
+
+  const keyboardEvents = (e) => {
+    if (e.key === 'ArrowLeft') checkAnswer(false);
+    if (e.key === 'ArrowRight') checkAnswer(true);
+  };
+
+  useEffect(() => {
+    setWord(generateNewWord(counter));
+  }, [setWord, counter]);
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyboardEvents);
+    return () => {
+      window.removeEventListener('keydown', keyboardEvents);
+    };
+  }, [keyboardEvents]);
+
+  return (
     <Box>
-      <Timer seconds={60}/>
-      <Streak />
-      <h1>{word}</h1>
-      <h2>{translation}</h2>
+      words used {counter}
+      <Timer seconds={60} />
+      <Streak streak={streak} />
+      <h1>{word.typing}</h1>
+      <h2>{word.shownTranslate}</h2>
       <div>
-        <button>верно</button>
-        <button>не верно</button>
+        <button onClick={() => checkAnswer(false)}>не верно</button>
+        <button onClick={() => checkAnswer(true)}>верно</button>
       </div>
     </Box>
-  )
-}
+  );
+};
 
 export default SprintGame;
