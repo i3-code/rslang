c6524fad52320d5ef88ecb575bc0927a06cbb214
i3 <i3pobox@gmail.com>
2021-03-25 15:56:03 +0300

    refactor: code style

--- rslang/src/app/Games/Savannah/ResultGame/ResultGame.js
+++ rslang/src/app/Games/Savannah/ResultGame/ResultGame.js
@@ -1,21 +1,25 @@
-import './ResultGame.css'
-import {NavLink} from 'react-router-dom'
-import React from "react";
-import {useSelector, useDispatch} from 'react-redux'
-import {restartGame, selectResult} from '../savannahSlice'
+import './ResultGame.css';
+import { NavLink } from 'react-router-dom';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { restartGame, selectResult } from '../savannahSlice';
 
-const ResultGame = ({result}) => {
-  const dispatch = useDispatch()
+const ResultGame = ({ result }) => {
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper-result-game">
       <div className="result-game-percent">
         <div>Ваш результат {useSelector(selectResult)}%</div>
       </div>
-      <div className="button-continue" onClick={() => dispatch(restartGame())}>Продолжить тренеровку</div>
-      <NavLink className="button-back" to="/games">К списку тренеровок</NavLink>
+      <div className="button-continue" onClick={() => dispatch(restartGame())}>
+        Продолжить тренеровку
+      </div>
+      <NavLink className="button-back" to="/games">
+        К списку тренеровок
+      </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default ResultGame
+export default ResultGame;
