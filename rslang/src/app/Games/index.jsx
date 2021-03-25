533524924b6c22242ba82f9f8b19eef374d86646
i3 <i3pobox@gmail.com>
2021-03-25 14:40:26 +0300

    feat: add links to game cards

--- rslang/src/app/Games/index.jsx
+++ rslang/src/app/Games/index.jsx
@@ -3,10 +3,10 @@ import { Container, Grid } from '@material-ui/core';
 import Card from '../../components/partials/Card';
 
 const cardsArray = [
-  { img: './images/1.jpg', name: 'Саванна', background: 'linear-gradient(45deg,#F9F53E,#FBF969)' },
-  { img: './images/2.jpg', name: 'Аудиовызов', background: 'linear-gradient(45deg,#4099ff,#73b4ff)' },
-  { img: './images/3.jpg', name: 'Спринт', background: 'linear-gradient(45deg,#FF5370,#ff869a)' },
-  { img: './images/4.jpg', name: 'Своя игра', background: 'linear-gradient(45deg,#2ed8b6,#59e0c5)' },
+  { img: './images/1.jpg', name: 'Саванна', background: 'linear-gradient(45deg,#F9F53E,#FBF969)', link: '/games/savannah'},
+  { img: './images/2.jpg', name: 'Аудиовызов', background: 'linear-gradient(45deg,#4099ff,#73b4ff)', link: '/games/audiocall' },
+  { img: './images/3.jpg', name: 'Спринт', background: 'linear-gradient(45deg,#FF5370,#ff869a)', link: '/games/sprint' },
+  { img: './images/4.jpg', name: 'Своя игра', background: 'linear-gradient(45deg,#2ed8b6,#59e0c5)', link: '/games/ourgame' },
 ];
 
 export default function Games() {
