d1ea2507eaa22bfd00168e534178c037ae863d5d
volha010892 <66121147+volha010892@users.noreply.github.com>
2021-03-25 16:39:09 +0300

    refactor: change images

--- rslang/src/app/Games/index.jsx
+++ rslang/src/app/Games/index.jsx
@@ -4,10 +4,10 @@ import Card from '../../components/partials/Card';
 import Savannah from './Savannah';
 
 const cardsArray = [
-  { img: './images/1.jpg', name: 'Саванна', background: 'linear-gradient(45deg,#F9F53E,#FBF969)', link: '/games/savannah'},
-  { img: './images/2.jpg', name: 'Аудиовызов', background: 'linear-gradient(45deg,#4099ff,#73b4ff)', link: '/games/audiocall' },
-  { img: './images/3.jpg', name: 'Спринт', background: 'linear-gradient(45deg,#FF5370,#ff869a)', link: '/games/sprint' },
-  { img: './images/4.jpg', name: 'Своя игра', background: 'linear-gradient(45deg,#2ed8b6,#59e0c5)', link: '/games/ourgame' },
+  { img: './images/games/savanna.jpg', name: 'Саванна', background: 'linear-gradient(45deg,#F9F53E,#FBF969)', link: '/games/savannah'},
+  { img: './images/games/audio.jpg', name: 'Аудиовызов', background: 'linear-gradient(45deg,#4099ff,#73b4ff)', link: '/games/audiocall' },
+  { img: './images/games/sprint.jpg', name: 'Спринт', background: 'linear-gradient(45deg,#FF5370,#ff869a)', link: '/games/sprint' },
+  { img: './images/games/sprint.jpg', name: 'Своя игра', background: 'linear-gradient(45deg,#2ed8b6,#59e0c5)', link: '/games/ourgame' },
 ];
 
 const games = {
