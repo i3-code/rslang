c5312ca09741d8fcc4e88d08ecdd376305c314c2
i3-code <i3pobox@gmail.com>
2021-03-27 09:16:58 +0300

    refactor: change router to hash-touter

--- rslang/src/App.js
+++ rslang/src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Loading from './components/partials/Loading';
 
