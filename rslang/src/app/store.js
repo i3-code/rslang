db86c954faecd338f10650f33d163d2b499c0fde
i3-code <i3pobox@gmail.com>
2021-03-31 16:18:54 +0300

    refactor: cleanup

--- rslang/src/app/store.js
+++ rslang/src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import savannahGameReducer from './Games/Savannah/savannahSlice';
 import userReducer from '../redux/userSlice';
 import appReducer from '../redux/appSlice';
@@ -7,7 +6,6 @@ import bookReducer from './Book/bookSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     savannahGame: savannahGameReducer,
     user: userReducer,
     app: appReducer,
