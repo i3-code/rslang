e37123605bbc524370b8ce6126d50483ec6d0ece
i3-code <i3pobox@gmail.com>
2021-04-02 11:46:15 +0300

    refactor: code cleanup

--- rslang/src/app/Dictionary/index.jsx
+++ rslang/src/app/Dictionary/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+
+import {deletedWords, hardWords} from '../../redux/appSlice';
+
+import useStyles from './styles';
+import { useSelector } from 'react-redux';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -15,39 +18,21 @@ function TabPanel(props) {
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
       {value === index && (
         <Box p={3}>
-          <Typography>{children}</Typography>
+          {children}
         </Box>
       )}
     </div>
   );
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
 export default function SimpleTabs() {
+  const delW = useSelector(deletedWords);
+  const hardW = useSelector(hardWords);
+
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -59,13 +44,13 @@ export default function SimpleTabs() {
     <div className={classes.root}>
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Изучаемые слова" {...a11yProps(0)} />
-          <Tab label="Сложные слова" {...a11yProps(1)} />
-          <Tab label="Удалённые слова" {...a11yProps(2)} />
+          <Tab label="Изучаемые слова" />
+          <Tab label="Сложные слова" />
+          <Tab label="Удалённые слова" />
         </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
-        Item One
+      <TabPanel value={value} index={0} >
+        <div>AAA</div>
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
