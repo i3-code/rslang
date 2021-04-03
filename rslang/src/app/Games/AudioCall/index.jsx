00511cc5ea412f700161daf5fb568a66fe46971f
Denis Korzun <dzianiskor@gmail.com>
2021-04-03 16:05:06 +0300

    fix: some problem with double sound in AudioCall

--- rslang/src/app/Games/AudioCall/index.jsx
+++ rslang/src/app/Games/AudioCall/index.jsx
@@ -30,7 +30,6 @@ export default function AudioCall() {
 
   useEffect(() => {
     if (start) {
-      console.log('START');
       (async () => {
         await dispatch(fetchWordsForQuiz(urls.words.all));
       })();
