1b53c84391c8c936b5ce1768a6aaacb4ac7f2b0b
Denis Korzun <dzianiskor@gmail.com>
2021-04-08 17:05:51 +0300

    feat: progress bars for games

--- rslang/src/app/Games/Savannah/SavannahQuiz/SavannahQuiz.js
+++ rslang/src/app/Games/Savannah/SavannahQuiz/SavannahQuiz.js
@@ -1,11 +1,15 @@
 import SavannahQuestion from './SavannahQuestion/SavannahQuestion';
 import SavannahAnswers from './SavannahAnswers/SavannahAnswers';
+import LinearDeterminate from "../../components/LinearDeterminate/LinearDeterminate";
+import Circular from "../../components/Circular/Circular";
 
 const SavannahQuiz = () => {
   return (
     <>
+      <LinearDeterminate/>
       <SavannahQuestion />
       <SavannahAnswers />
+      <Circular/>
     </>
   );
 };
---
+++ rslang/src/app/Games/components/Circular/Circular.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { useSelector } from 'react-redux';
+import { selectPercentRightAnswers } from '../../Savannah/savannahSlice';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    position: 'absolute',
+    bottom: '15%',
+    textAlign: 'center',
+  },
+  title: {
+    color: 'white',
+    paddingBottom: '10px',
+  },
+  typography: {
+    color: 'white',
+    fontSize: '18px',
+  },
+});
+
+function CircularProgressWithLabel(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.title}>Правильных ответов:</div>
+      <Box position="relative" display="inline-flex">
+        <CircularProgress style={{ width: '80px', height: '80px' }} variant="determinate" {...props} />
+        <Box
+          top={0}
+          left={0}
+          bottom={0}
+          right={0}
+          position="absolute"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography
+            className={classes.typography}
+            variant="caption"
+            component="div"
+            color="textSecondary"
+          >{`${Math.round(props.value)}%`}</Typography>
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
+export default function Circular() {
+  const progress = useSelector(selectPercentRightAnswers);
+
+  return <CircularProgressWithLabel value={progress} />;
+}
